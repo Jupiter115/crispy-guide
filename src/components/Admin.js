5010f6f34a73d4342c6abeff04d60ed7c06f5fcb
Stephen Wong <wongstephenky@gmail.com>
2022-08-04 16:15:51 -0700

    finished admin

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Admin() {
-  return <div>Admin</div>;
+  return (
+    <div>
+      Admin Enter your password
+      <form>
+        <input type="text"></input>
+        <button type="submit">Enter</button>
+      </form>
+    </div>
+  );
 }
