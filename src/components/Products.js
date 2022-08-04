bed1c0b658c4d6583ab5050becd1a16a9ee812c9
Stephen Wong <wongstephenky@gmail.com>
2022-08-04 16:34:29 -0700

    product pages

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,20 @@
 import React from "react";
-import Product_card from "./Product_card";
+import ProductCard from "./ProductCard";
+import products from "../data/seeds.json";
 
 export default function Products() {
   return (
     <div>
-      Products
-      <Product_card />
+      {products.map((product, index) => (
+        <ProductCard
+          key={index}
+          title={product.title}
+          description={product.description}
+          imageUrl={product.image}
+          salePrice={product["sale-price"]}
+          origPrice={product["orig-price"]}
+        />
+      ))}
     </div>
   );
 }
