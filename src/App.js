b0b6793798331f3a85bf6129509dabd23607539c
Stephen Wong <wongstephenky@gmail.com>
2022-08-04 15:35:52 -0700

    added admin panel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Products from "./components/Products";
+import { Route, Routes } from "react-router-dom";
+import Admin from "./components/Admin";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Products />
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Product_card.js
+++ src/components/Product_card.js
@@ -4,7 +4,19 @@ export default function Product_card() {
   return (
     <main>
       Product Card Component
-      <img src="https://placeholder.pics/svg/300"></img>
+      <img src="https://placeholder.pics/svg/300" />
+      <h3 className="Product_card_title">Product Title</h3>
+      <p className="Product_card_body">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <p className="Product_card_price-original">$1000</p>
+      <p className="Product_card_price-sale">$500</p>
     </main>
   );
 }
