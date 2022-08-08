d9e882e326b9e6b3dec723cdc3b0015724c3d68a
Fred0624 <fred10150912@hotmail.com>
2022-08-08 16:40:59 -0700

    product-description

--- src/components/ProductDescription.js
+++ src/components/ProductDescription.js
@@ -1,30 +1,50 @@
-import { Container, Card } from 'react-bootstrap'
-import React from 'react'
+import { Container, Card, Button } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
 
-export default function ProductDescription(props) {
+export default function ProductDescription() {
+  const [product, setProduct] = useState([])
+  const { id } = useParams()
+
+useEffect(() => {
+  axios
+  .get(`https://mysterious-temple-52384.herokuapp.com/products/${id}`)
+  .then((res) => setProduct(res.data))
+},[])
+
   return (
     <Container>
         <Card className = "description">
 
-            <Card.Img src = "https://m.media-amazon.com/images/I/61ku9UiTwkL._AC_SX569_.jpg" />
+            <Card.Img src = {product.image} />
 
             <Card.Title>
-
+            {product.title}
             </Card.Title>
 
 
             <Card.Body>
                 <Card.Text> 
-                "Funko Pop! Marvel: Spider-Man: No Way Home - Spider-Man en traje integrado"
-
-
+                  Description: {product.description}
+                </Card.Text>
+                <Card.Text>
+                  Original Price: {product.orig}
+                </Card.Text>
+                <Card.Text>
+                  Sale Price: {product.price}
                 </Card.Text>
             </Card.Body>
 
-
+            <Button>
+              <a href={product.link}> See Jupiter Deal </a>
+            </Button>
 
         </Card>
 
+        <Button>
+          <Link to={`/admin`}>Admin</Link>
+        </Button>
 
 
     </Container>
