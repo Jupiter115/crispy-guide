f79dd3b88ebbd261cf863357f0d3d08b7ae5232d
Stephen Wong <wongstephenky@gmail.com>
2022-08-08 15:50:44 -0700

    Updated header (#12)
    * css
    
    * header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import jupiter from "../assets/jupiter-900656-removebg-preview-removebg-preview.png";
 
 export default function Header() {
   return (
     <header>
+      <div>
+        <img src={jupiter} alt="logo" className="header_logo" />
+      </div>
+
       <h1 className="header_title">
         <Link to="/">Jupiter Deals</Link>
       </h1>
+      <p className='header_subtitle'>The sales are out of this world.</p>
     </header>
   );
 }
