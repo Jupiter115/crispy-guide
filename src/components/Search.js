c11a9c85268d342f234a6e12ba00803b6a5bf506
mattstudzinski <matthewstudzinski@yahoo.com>
2022-08-09 12:01:34 -0700

    search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,19 @@
-
 import React, { useState } from "react";
 import { Container, Form } from "react-bootstrap";
 
-export default function Search({ handleChange, handleSubmit, search }) {
+export default function Search({handleChange,handleSubmit,search}) {
+
+  
   return (
     <Container>
-      <Form.Control
-        type="text"
-        placeholder="search"
-        value={search}
-        onChange={handleChange}
-        onSubmit={handleSubmit}
-        style={{ margin: "0px 0px 30px 0px" }}
-      />
+      <Form.Control 
+       type="text"
+       placeholder="search"
+       value={search}
+       onChange={handleChange}
+       onSubmit={handleSubmit}
+       style={{ margin: "0px 0px 30px 0px" }}
+       />
     </Container>
   );
 }
