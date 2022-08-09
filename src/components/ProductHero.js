5799e2c74c8c252ea4754dd4e83e3c252c005d39
CodingMauri <106369465+CodingMauri@users.noreply.github.com>
2022-08-09 14:40:27 -0700

    save

--- src/components/ProductHero.js
+++ src/components/ProductHero.js
@@ -1,34 +1,30 @@
+import React, { useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import "bootstrap/dist/css/bootstrap.css";
 
-
-
-import React, { useState } from "react"
-import  Carousel  from "react-bootstrap/Carousel";
-import 'bootstrap/dist/css/bootstrap.css';
-import { CarouselItem } from "react-bootstrap";
-
+import { Paper } from "@mui/material";
 
 export default function ProductHero(props) {
-
-  const [index,setIndex] = useState(0)
-  const handleSelect = (selectedIndex , e) => {
-    setIndex(selectedIndex)
-  }
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   const filteredArray = props.data.filter((product) => {
-  return product.hero === true;
+    return product.hero === true;
   });
-  
 
   return (
-    <>
     <Carousel className="carousel" activeIndex={index} onSelect={handleSelect}>
-      {...filteredArray.forEach(hero => {
-        <CarouselItem>
-         <img src = {hero.image} />
-         <Carousel.Caption>
-          <h3>{hero.title}</h3>
-         </Carousel.Caption>
-         </CarouselItem>
-        })}
+      {filteredArray.map((feature) => {
+        return (
+            <div className="slide">
+              <img className="carousel-img" src={feature.image} alt="product">
+              </img>
+                <h3>{feature.title}</h3>
+            </div>
+        
+        );
+      })}
     </Carousel>
-    </>
-  )
+  );
+}
